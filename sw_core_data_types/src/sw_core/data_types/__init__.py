0266d495e0e44d82e33efb5ba7b8a704d55b2b63
StudioWEngineers <studio.w.engineers@gmail.com>
2024-03-07 22:45:07 +0100

    add logger to __init__.py

---
+++ sw_core_data_types/src/sw_core/data_types/__init__.py
@@ -0,0 +1,29 @@
+"""
+MutableString
+-------------
+
+The `MutableString` class represents a mutable string object.
+
+This class mimics a Python string object, but being mutable at the same time. It stores
+the content of the string and provides some of the methods that the original Python `str`
+class provides.
+
+__author__ = "Studio W Engineers"
+
+__version__ = "0.0.0"
+
+__maintainer__ = "Studio W Engineers"
+
+__email__ = "studio.w.engineers@gmail.com"
+
+__status__ "Release to manufacturing"
+"""
+# standard library imports
+import logging
+
+# third party library imports
+
+# local library specific imports
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
